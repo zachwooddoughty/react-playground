c7c8ca9df15149d375f842a0c61942bfc71656ab
Zach Wood-Doughty <zachwooddoughty@users.noreply.github.com>
2015-07-21 15:37:56 -0700

    Delete secrets.py

--- secrets.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ['dbo-userid'] = "54d55f71d67361000dd83672"
-os.environ['dbo-sessionKey'] = "tddtnrndbmgzvwwxockirdotdvlutsowbimceocwyahoegfzfccdblheffpnmkrpkuvpjkkybdresdzwitwzbaemeghhycxvpwzdchhrjbquiatvaxhdggmyiofyimqv"
