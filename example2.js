0255b4a23ec6c6e33c6dc2c41c250f28fad1b916
zachwooddoughty <zachwooddoughty@gmail.com>
2015-07-20 17:29:03 -0700

    Two column infinite scroll is working!

--- example2.js
+++ example2.js
@@ -2,24 +2,28 @@ var TableBody = React.createClass({displayName: "TableBody",
     getInitialState: function() {
         return {
             shouldUpdate: true,
-            totalLength: 0,
-            displayStart: 0,
-            displayEnd: 0
+            firstRenderedPixel: 0,
+            lastRenderedPixel: 0,
+            scrollBuffer: 500
         };
     },
 
     componentWillReceiveProps: function(nextProps) {
-        var shouldUpdate =
-            nextProps.visibleStart < this.state.displayStart
-            || nextProps.visibleEnd > this.state.displayEnd
-            || nextProps.totalLength !== this.state.totalLength;
+        var scrollTop = nextProps.scrollPosition;
+        var scrollBottom = this.state.firstRenderedPixel + this.props.height + nextProps.scrollPosition;
+        var shouldUpdate = 
+            scrollTop < this.state.firstRenderedPixel + this.state.scrollBuffer
+            || scrollBottom > this.state.lastRenderedPixel - this.state.scrollBuffer;
 
+        console.log("Is " + scrollTop + "<" + this.state.firstRenderedPixel + "+500? "
+        + "Is " + scrollBottom + ">" + this.state.lastRenderedPixel + "-500? "
+        + "Should we update? " + (shouldUpdate ? "yup!" : "nope.")
+        );
         if (shouldUpdate) {
             this.setState({
                 shouldUpdate: shouldUpdate,
-                totalLength: nextProps.totalLength,
-                displayStart: nextProps.displayStart,
-                displayEnd: nextProps.displayEnd
+                firstRenderedPixel: nextProps.firstRenderedPixel,
+                lastRenderedPixel: nextProps.lastRenderedPixel
             });
         } else {
             this.setState({shouldUpdate: false});
@@ -31,28 +35,54 @@ var TableBody = React.createClass({displayName: "TableBody",
     },
 
     render: function(){
-        var rows = []
+        var rowHeight = 50;
         var tdStyle = {border: '1px solid white', width: '50%', background: '#CCCCCC'};
-        var trStyle = {height: '50px'};
+        var trStyle = {height: rowHeight + 'px'};
 
-        console.log("Displaying " + (this.props.displayEnd - this.props.displayStart) + " rows in our table");
+        console.log("Displaying from " + this.props.firstRenderedPixel + " to " + this.props.lastRenderedPixel);
+        var rows = []
 
-        for (var i = this.props.displayStart; i < this.props.displayEnd; ++i) {
+        var leftDepth = 0,
+            rightDepth = 0,
+            leftOverhang = 0,
+            rightOverhang = 0;
+
+        var i = 0;
+        while (i < this.props.items.length && Math.min(leftDepth, rightDepth) < this.props.lastRenderedPixel){
             var itemList = this.props.items[i];
             var row;
             if (itemList.length == 0){
+                if (leftOverhang > 0) {leftOverhang -= 1;}
+                if (rightOverhang > 0) { rightOverhang -= 1;}
                 row = "";
             } else if (itemList.length == 1){
                 item = itemList[0];
+                if (leftDepth <= rightDepth){
+                    leftDepth += item * rowHeight;
+                    leftOverhang = item - 1;
+                } else {
+                    rightDepth += item * rowHeight;
+                    rightOverhang = item - 1;
+                }
                 row = (React.createElement("td", {style: tdStyle, rowSpan: item}, item));
             } else {
                 item1 = itemList[0];
+                leftDepth += item1 * rowHeight;
+                leftOverhang = item1 - 1;
                 item2 = itemList[1];
+                rightDepth += item2 * rowHeight;
+                rightOverhang = item2 - 1;
                 row = (React.createElement("div", null, React.createElement("td", {style: tdStyle, rowSpan: item1}, item1), React.createElement("td", {style: tdStyle, rowSpan: item2}, item2)));
             }
-            rows.push(
-                (React.createElement("tr", {style: trStyle, key: i}, row))
-            );
+            if (Math.max(leftDepth, rightDepth) > this.props.firstRenderedPixel){
+                rows.push(
+                    (React.createElement("tr", {style: trStyle, key: i}, row))
+                );
+            }
+            i++;
+            if (i >= this.props.items.length){
+                console.log("Oh no! We've run out of items!");
+            }
         }
         return(
             React.createElement("table", {style: {width: '100%'}}, 
@@ -65,37 +95,30 @@ var TableBody = React.createClass({displayName: "TableBody",
 
 var InfiniteTable = React.createClass({displayName: "InfiniteTable",
     getInitialState: function(){
-        var chunkHeight = 50 * 13;
-        var chunksPerPage = 1;  // Math.floor(this.props.height / chunkHeight);
+        var itemsPerPage = 20;  // Math.floor(this.props.height / chunkHeight);
         return {
             items: this.props.items,
             totalLength: this.props.items.length,
             height: this.props.height,
-            itemsPerChunk: 13,
-            chunksPerPage: chunksPerPage,
-            visibleStart: 0,
-            visibleEnd: 13 * chunksPerPage,
-            displayStart: 0,
-            displayEnd: 13 * chunksPerPage * 3
+            itemsPerPage: itemsPerPage,
+            // firstLoadedPixel: 0,
+            // lastLoadedPixel: 5000,
+            firstRenderedPixel: 0,
+            lastRenderedPixel: 3000,
+            scrollPosition: 0,
         };
     },
 
-    scrollState: function(scroll) {
-        console.log("We've scrolled to " + scroll);
-        var itemsPerChunk = this.state.itemsPerChunk;
+    scrollState: function(scrollPosition) {
+        // ZACHTODO: This needs to exactly represent where in the scrolling-world this should be.
 
-        var visibleStart = itemsPerChunk * Math.floor(scroll / this.state.chunksPerPage);
-        var visibleEnd = Math.min(itemsPerChunk * (visibleStart + 2), this.state.totalLength - 1);
-
-        var displayStart = Math.max(0, itemsPerChunk * (Math.floor(scroll / this.state.chunksPerPage) - 1));
-        var displayEnd = Math.min(displayStart + 3 * itemsPerChunk * this.state.chunksPerPage, this.state.totalLength - 1);
+        var firstRenderedPixel = Math.max(0, scrollPosition - 3000);
+        var lastRenderedPixel = Math.min(scrollPosition + 3000, 8000);
 
         this.setState({
-            visibleStart: visibleStart,
-            visibleEnd: visibleEnd,
-            displayStart: displayStart,
-            displayEnd: displayEnd,
-            scroll: scroll
+            firstRenderedPixel: firstRenderedPixel,
+            lastRenderedPixel: lastRenderedPixel,
+            scrollPosition: scrollPosition,
         });
     },
 
@@ -105,6 +128,8 @@ var InfiniteTable = React.createClass({displayName: "InfiniteTable",
 
     componentDidMount: function(){
         window.addEventListener('scroll', this.onScroll);
+        // Scroll down to hide the "Loading..." header
+        // document.body.scrollTop = 50;
     },
 
     componentWillUnmount: function(){
@@ -116,11 +141,10 @@ var InfiniteTable = React.createClass({displayName: "InfiniteTable",
             React.createElement("div", {style: {'overflowX': 'hidden', 'overflowY': 'auto', top: 26}, ref: "scrollable"}, 
                 React.createElement(TableBody, {
                     items: this.state.items, 
-                    totalLength: this.state.totalLength, 
-                    visibleStart: this.state.visibleStart, 
-                    visibleEnd: this.state.visibleEnd, 
-                    displayStart: this.state.displayStart, 
-                    displayEnd: this.state.displayEnd}
+                    firstRenderedPixel: this.state.firstRenderedPixel, 
+                    lastRenderedPixel: this.state.lastRenderedPixel, 
+                    scrollPosition: this.state.scrollPosition, 
+                    height: this.state.height}
                 )
             )
         );
@@ -135,7 +159,7 @@ for (i = 0; i < 1000; i++){
 React.render(
     React.createElement(InfiniteTable, {
         items: items, 
-        height: 600}
+        height: window.innerHeight}
     ),
     document.getElementById('infinite-table')
 );
